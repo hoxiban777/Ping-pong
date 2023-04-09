18857d5aedbe495889e51625e06f3169c48dd002
hoxiban777 <129663716+hoxiban777@users.noreply.github.com>
2023-04-09 15:36:59 +0300

    v1.1
    Добавил ракетки с перемещением. Отрисовал мяч

--- main.py
+++ main.py
@@ -23,14 +23,28 @@ class GameSprite(sprite.Sprite):
         background.blit(self.image, (self.rect.x, self.rect.y))
 
 class Player(GameSprite):
-    def update(self):
+    def update1(self):
         key_pressed = key.get_pressed()
-        if key_pressed[K_RIGHT] and self.rect.x < 1200 - 130 - 5:
-            self.rect.x += speed
-        if key_pressed[K_LEFT] and self.rect.x > 5:
-            self.rect.x -= speed
+        if key_pressed[K_UP] and self.rect.y > 5:
+            self.rect.y -= speed
+        if key_pressed[K_DOWN] and self.rect.y < 800 - 130 - 5:
+            self.rect.y += speed
+    def update2(self):
+        key_pressed = key.get_pressed()
+        if key_pressed[K_w] and self.rect.y > 5:
+            self.rect.y -= speed
+        if key_pressed[K_s] and self.rect.y < 800 - 130 - 5:
+            self.rect.y += speed
 
+class Ball(GameSprite):
+    def update(self):
+        self.rect.x += self.speed
+        self.rect.y += self.speed
 
+raketka1 = Player('raketka.png', 1000, 300, speed, 150, 150)
+raketka2 = Player('raketka.png', 80, 300, speed, 150, 150)
+
+myach = Ball('myach.png', 500, 300, speed, 150, 150)
 
 lose = font.render('Ты не выиграл', True, (255, 255, 255))
 
@@ -42,6 +56,11 @@ finish = False
 while game:
     if finish != True:
         background.fill((0, 255, 255))
+        raketka1.reset()
+        raketka1.update1()
+        raketka2.reset()
+        raketka2.update2()
+        myach.reset()
     for e in event.get():
         if e.type == QUIT:
             game = False

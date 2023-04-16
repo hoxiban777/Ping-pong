979c77989d32b65774bdc732cf35c045da0088a2
hoxiban777 <129663716+hoxiban777@users.noreply.github.com>
2023-04-16 15:05:13 +0300

    v2
    Добавил передвижение мяча и обработал все события. Обработал проигрыш каждого игрока.

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ font = font.Font(None, 70)
 background = display.set_mode((1200, 800))
 display.set_caption('Пинг-понг')
 
-
 speed = 10
 
 class GameSprite(sprite.Sprite):
@@ -46,25 +45,39 @@ raketka2 = Player('raketka.png', 80, 300, speed, 150, 150)
 
 myach = Ball('myach.png', 500, 300, speed, 150, 150)
 
-lose = font.render('Ты не выиграл', True, (255, 255, 255))
+lose = font.render('Игрок 1 проиграл', True, (255, 255, 255))
+lose2 = font.render('Игрок 2 проиграл', True, (255, 255, 255))
 
 clock = time.Clock()
 fps = 60
 game = True
 finish = False
 
+speedx = 6
+speedy = 6
 while game:
     if finish != True:
+        myach.rect.x += speedx
+        myach.rect.y += speedy
         background.fill((0, 255, 255))
         raketka1.reset()
         raketka1.update1()
         raketka2.reset()
         raketka2.update2()
         myach.reset()
+        if myach.rect.y > 800 - 130 or myach.rect.y < 0:
+            speedy *= -1
+        if myach.rect.x > 1200 - 130:
+            finish = True
+            background.blit(lose2, (500, 300))
+        if myach.rect.x < 0:
+            finish = True
+            background.blit(lose, (500, 300))
+        if sprite.collide_rect(myach, raketka1) or sprite.collide_rect(myach, raketka2):
+            speedx *= -1
     for e in event.get():
         if e.type == QUIT:
             game = False
-
 
 
 
